0b7d8f02917740aeb24519c5794fed104b18cf17
TobiWan8 <tobiaslarsen93@gmail.com>
2024-05-11 12:12:52 +0200

    Pages, navmenu and folder structure

--- CarShareBlazor/Components/AuthCusom/MustBeAuthorizedWithoutPersonRequirement.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.AspNetCore.Authorization;
-
-namespace CarShareBlazor.Components.AuthCusom
-{
-    public class MustBeAuthorizedWithoutPersonRequirement : IAuthorizationRequirement
-    {
-    }
-}
