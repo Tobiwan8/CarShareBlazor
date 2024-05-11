954c3ac3a351b96042bef5da466d556e278b774b
TobiWan8 <tobiaslarsen93@gmail.com>
2024-05-11 13:22:01 +0200

    profilePage

--- CarShareBlazor/Models/Entities/PersonModel.cs
+++ CarShareBlazor/Models/Entities/PersonModel.cs
@@ -3,7 +3,7 @@
     public class PersonModel
     {
         public int ID { get; set; }
-        public string? Name { get; set; }
+        public string? FirstName { get; set; }
         public string? LastName { get; set; }
         public int UserID { get; set; }
     }
