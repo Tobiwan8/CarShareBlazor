9388d7e9f63a23180abcbed774aced566b6b8f89
TobiWan8 <tobiaslarsen93@gmail.com>
2024-05-12 17:41:06 +0200

    Edit Profile and remove rendermode on navmenu

---
+++ CarShareBlazor/Models/Services/LogOutService.cs
@@ -0,0 +1,16 @@
+﻿namespace CarShareBlazor.Models.Services
+{
+    public class LogOutService
+    {
+        private bool _isLoggedOut = false;
+        public bool IsLoggedOut => _isLoggedOut;
+
+        public event Action? OnLoggedOut;
+
+        public void SetLoggedOut(bool value)
+        {
+            _isLoggedOut = value;
+            OnLoggedOut?.Invoke();
+        }
+    }
+}
