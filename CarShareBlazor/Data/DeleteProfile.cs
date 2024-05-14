bc3e950cafd5b912fa8fd167ad78b44b61aad7b0
TobiWan8 <tobiaslarsen93@gmail.com>
2024-05-14 11:30:19 +0200

    Delete profile

---
+++ CarShareBlazor/Data/DeleteProfile.cs
@@ -0,0 +1,25 @@
+﻿using CarShareBlazor.Models.DTOs;
+using CarShareBlazor.Models.Entities;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarShareBlazor.Data
+{
+    public class DeleteProfile
+    {
+        public async Task<string?> DeleteUserAsync(int userID)
+        {
+            using HttpClient client = new()
+            {
+                BaseAddress = new Uri(APIRoot.APIRootAddress)
+            };
+
+            HttpResponseMessage response = await client.DeleteAsync($"api/User?userID={userID}");
+            if (response.IsSuccessStatusCode)
+            {
+                return "ok";
+            }
+            return null;
+        }
+    }
+}
