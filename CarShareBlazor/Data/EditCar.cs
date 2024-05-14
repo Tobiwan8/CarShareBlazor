6ac813306e45476bff926ec6d9e09adf52371b5b
TobiWan8 <tobiaslarsen93@gmail.com>
2024-05-14 10:55:58 +0200

    EditCar

---
+++ CarShareBlazor/Data/EditCar.cs
@@ -0,0 +1,27 @@
+﻿using CarShareBlazor.Models.DTOs;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarShareBlazor.Data
+{
+    public class EditCar
+    {
+        public async Task<string?> EditCarAsync(EditCarDTO car)
+        {
+            using HttpClient client = new()
+            {
+                BaseAddress = new Uri(APIRoot.APIRootAddress)
+            };
+
+            var json = JsonConvert.SerializeObject(car);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PutAsync("api/Car", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return "ok";
+            }
+            return null;
+        }
+    }
+}
---
+++ CarShareBlazor/Models/DTOs/EditCarDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CarShareBlazor.Models.DTOs
+{
+    public class EditCarDTO
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Licenseplate { get; set; }
+    }
+}
