91a074890ffca7d351b1ac47ab0b52f0af7bce45
TobiWan8 <tobiaslarsen93@gmail.com>
2024-05-17 15:37:47 +0200

    grid for bookings

---
+++ CarShareBlazor/Data/GetPersonBookings.cs
@@ -0,0 +1,27 @@
+﻿using CarShareBlazor.Models.DTOs;
+using Newtonsoft.Json;
+
+namespace CarShareBlazor.Data
+{
+    public class GetPersonBookings
+    {
+        public async Task<List<PersonBookingDTO>?> GetPersonBookingsAsync(int personID)
+        {
+            using HttpClient client = new()
+            {
+                BaseAddress = new Uri(APIRoot.APIRootAddress)
+            };
+
+            var requestUrl = $"/api/Booking/personID/{personID}";
+
+            HttpResponseMessage response = await client.GetAsync(requestUrl);
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<List<PersonBookingDTO>>(responseContent);
+            }
+            return null;
+        }
+    }
+}
---
+++ CarShareBlazor/Models/DTOs/PersonBookingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CarShareBlazor.Models.DTOs
+{
+    public class PersonBookingDTO
+    {
+        public int ID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string? CarName { get; set; }
+    }
+}
