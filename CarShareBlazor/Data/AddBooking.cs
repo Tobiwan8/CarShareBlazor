6cac8c7c9e54081da4ca9948c619d7e9325e830f
Tobias K. Larsen <tobiaslarsen93@gmail.com>
2024-05-18 15:59:57 +0200

    Add Car by car id

---
+++ CarShareBlazor/Data/AddBooking.cs
@@ -0,0 +1,27 @@
+﻿using CarShareBlazor.Models.DTOs;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace CarShareBlazor.Data
+{
+    public class AddBooking
+    {
+        public async Task<string?> AddBookingAsync(AddBookingDTO booking)
+        {
+            using HttpClient client = new()
+            {
+                BaseAddress = new Uri(APIRoot.APIRootAddress)
+            };
+
+            string json = JsonConvert.SerializeObject(booking);
+            StringContent content = new(json, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await client.PostAsync("api/Booking", content);
+            if (response.IsSuccessStatusCode)
+            {
+                return "ok";
+            }
+            return null;
+        }
+    }
+}
---
+++ CarShareBlazor/Models/DTOs/AddBookingDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace CarShareBlazor.Models.DTOs
+{
+    public class AddBookingDTO
+    {
+        public int PersonID { get; set; }
+        public int CarID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
